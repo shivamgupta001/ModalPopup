4d4f852ec86b139eb6eb7c19d3226f8b8b18a836
Shivam Gupta <shivamcs3080@gmail.com>
2016-11-25 18:28:33 +0530

    added template abd refactored as componenet

--- new popup/ModalPopup.js
+++ new popup/ModalPopup.js
@@ -1,6 +1,6 @@
 $(document).keyup(function(e) {
     var itemId = '';
-    var popups = $('.modal-outer');
+    var popups = $('.tf-modal-outer');
     if (popups.length > 0) {
         // retrieving id and data-zindex for all pop ups 
         var data = popups.map(function(index, value) {
@@ -23,115 +23,177 @@ $(document).keyup(function(e) {
             }
         }
         if (e.keyCode == 27) {
-            document.getElementById(itemId).querySelector('.modal-close-btn').click();
+            document.getElementById(itemId).querySelector('.tf-modal-close-btn').click();
         }
     }
 });
 
 ModalPopup = function(config) {
-    var modalPopup = {
+
+    var modalpopup = {
+
         scope: config,
         _init: function() {
 
             this._initialize();
+
             if (this._validateInitialize()) {
+
                 this._generateTemplate();
-                this._bind();
+                this._cacheDom();
+                this._applyProperty();
+                this._bindEvents();
+                this._attachProperties();
                 this._render();
                 this._handleKeyUp();
-                return Object.freeze({
-                    show: this.show.bind(this),
-                    close: this.close.bind(this),
-                    _id: this.itemId
-                });
+
+                return Object.freeze(this.outerComp);
             } else {
                 return false;
             }
         },
         _initialize: function() {
+
             var me = this.scope;
 
-            //caching variables
+                 //config
+                this.dynamicId = me.popupId || "tf-modal-" + getRandomInt(1, 10000),
+                this.onConfig = (me.onConfig == null || me.onConfig == false) ? false : true,
+                this.resizable = (me.resizable == false) ? false : true,
+                this.footerVisible = (me.footerVisible == false) ? false : true,
 
-            this.width = me.width || 800,
-            this.height = me.height || 600,
-            this.title = me.title || '',
-            this.itemId = me.popupId || "modal-Id-" + getRandomInt(1, 10000),
-            this.modalCloseCallback = me.modalOnClose || '',
-            this.modalOpenCallback = me.modalOnOpen || '',
-            this.$userTemplate = me.dataTemplate || null,
-            this.onConfig = (me.onConfig == false) ? false : true,
-            this.resizable = (me.resizable == false) ? false : true,
-            this.footerVisible =  (me.footerVisible == false) ? false : true,
-            this.$template = '',
-            this.modalBody = '',
-            this.modalCloseNode = '';
+                // style
+                this.width = me.width || 800,
+                this.height = me.height || 600,
+
+                // header text
+                this.title = me.title || '',
+
+                // callback
+                this.modalCloseCallback = me.modalOnClose || '',
+                this.modalOpenCallback = me.modalOnOpen || '',
+
+                // user Template
+                this.dataTemplate = me.dataTemplate || null
+                
 
         },
         _validateInitialize: function() {
-            if (document.body.querySelector('#' + this.itemId)) {
-                console.log('Duplicate Id ' + this.itemId);
+            if (document.getElementById(this.dynamicId)) {
+                console.log('Duplicate Id ' + this.dynamicId);
                 return false;
             }
             return true;
         },
         _generateTemplate: function() {
 
-            this.$template = $('<div id="' + this.itemId + '" class="modal-outer"> \
-                <section class="modal-inner"> \
-                    <header class="modal-header"> \
-                        <span class="modal-title">' + this.title + '</span>  \
-                        <a  class= "modal-close-btn"></a> \
-                    </header> \
-                    <section class="modal-body"></section> \
-                    "'+(this.footerVisible ?  '<footer class="modal-footer"></footer>':'')+'" \
-                    </section> \
-            </div>');
+            var el = [
+                '<div id="'+this.dynamicId+'" class="tf-modal-outer">',
+                    '<section class="tf-modal-inner">',
+                        // header    
+                        '<header class="tf-modal-header">',
+                            '<span class="tf-modal-title">' + this.title + '</span>',
+                            '<a  class= "tf-modal-close-btn"></a>',
+                        '</header>',
+                        //body
+                        '<section class="tf-modal-body"></section>',
+                        //footer
+                        '<footer class="tf-modal-footer ' + (this.footerVisible ? '' : 'tf-display--none' ) + '"></footer>',
+                    '</section>',
+                '</div>'
+            ].join('\n');
 
-
-            this.modalCloseNode = this.$template[0].querySelector('.modal-close-btn');
-            this.modalBody = this.$template[0].querySelector('.modal-body');
-            this.$template.attr("data-zindex", 1000 + $(".modal-outer").length);
-            if(this.$userTemplate)
-                $(this.modalBody).append(this.$userTemplate);
+            this.childTemplate = $(el)[0];
         },
-        _bind: function() {
-            this.modalCloseNode.addEventListener('click', this._handleModalCloseBtnClick.bind(this));
+        _cacheDom: function() {
+            
+            // cache Dom
+            this.outerComp = this.childTemplate;
+            this.modalCloseNode = this.childTemplate.querySelector('.tf-modal-close-btn');
+            this.modalBody = this.childTemplate.querySelector('.tf-modal-body');
+
+        },
+        _applyProperty : function(){
+
+            // data-zindex makes sure hierarchical modal popup closes in order of opening
+            var currentPopupCount = document.getElementsByClassName('.tf-modal-outer').length;
+            this.outerComp.setAttribute('data-zindex', (1000 + currentPopupCount));
+
+            // add data template if provided
+            if(this.dataTemplate){
+                this.modalBody.appendChild(this.dataTemplate);  
+            }else{
+                var tpl = document.querySelector('template[data-template-id="'+this.dynamicId+'"]')
+                if(tpl){
+                    var clone = document.importNode(tpl.content, true);
+                    this.modalBody.appendChild(clone);    
+                }
+                
+            } 
+        },
+        _bindEvents: function() {
+
+            var me = this.scope;
+            this._handleModalCloseBtnClick = this._handleModalCloseBtnClick.bind(this);
+            this.modalCloseNode.addEventListener('click', this._handleModalCloseBtnClick);
+        },
+        _attachProperties : function(){
+
+            var me = this.scope;
+
+            // add Properties 
+            me.outerComp = this.outerComp;
+
+            // add Methods
+            me.outerComp.show = this.show.bind(this);
+            me.outerComp.close = this.close.bind(this);
+
         },
         _render: function() {
-            if (!this.onConfig) {
-                $('body').append(this.$template);
+
+            if(!this.onConfig) {
+                
+                document.body.appendChild(this.outerComp);
                 this._handleModalOpenCallback();
             }
         },
         _handleModalOpenCallback: function() {
-            if (this.modalOpenCallback != '')
-                this.modalOpenCallback();
+            
+            var me = this.scope;
+
+            if(this.modalOpenCallback != '') this.modalOpenCallback.call(me);
         },
         _handleModalCloseBtnClick: function(e) {
-            if (this.modalCloseCallback != '')
-                this.modalCloseCallback();
+            
+            var me = this.scope;
+
+            if (this.modalCloseCallback != '')  this.modalCloseCallback.call(me);
             this._destroy();
         },
         _handleKeyUp: function(e) {
 
+
         },
         show: function() {
-            if ($('#' + this.itemId).length <= 0) {
-                $('body').append(this.$template);
-                this._handleModalOpenCallback();
+            
+            if(!document.getElementById(this.dynamicId)){
+
+                var me = this.scope;
+
+                document.body.appendChild(this.outerComp);
+                this._handleModalOpenCallback.call(this);
             }
         },
         close: function() {
-            if (document.getElementById(this.itemId)) {
+            
+            if(document.getElementById(this.dynamicId)) {
                 this.modalCloseNode.click();
             }
         },
         _destroy: function() {
 
-            $(this.$template).off();
-            $(this.$template).remove();
-
+            var oldChild = document.body.removeChild(document.getElementById(this.dynamicId));
+            oldChild.removeEventListener('click', this._handleModalCloseBtnClick);
         }
     };
 
@@ -140,6 +202,6 @@ ModalPopup = function(config) {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min);
     }
-    return modalPopup._init();
+    return modalpopup._init();
 
 }
